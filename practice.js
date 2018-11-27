b40a8f1614d5f27acafbb18e482e8bfad291784e
Allen Carlson <allen.carlson123@gmail.com>
2018-11-27 15:40:38 -0700

    1-5 complete

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(arr, cb) {
+  cb(arr[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, cb) {
+  cb(arr[arr.length - 1])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1, num2, cb) {
+  cb(num1 * num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +94,15 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr, name, cb) {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i]==name) {
+      cb(true)
+    } else {
+      cb(false)
+    }
+  }
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +124,17 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+
+function uniq(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j <arr.length; j++) {
+      if (arr[i] === arr[j] && i != j) {
+        arr.splice(i, 1)
+      }
+    }
+  }
+  cb(arr)
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
